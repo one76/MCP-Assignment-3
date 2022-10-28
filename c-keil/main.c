41a51503db1beaf39dc2f89d990f5793c0ad910a
one76 <michaeln2022@gmail.com>
2022-10-28 19:10:41 +1030

    MAIN 1 - formatted

--- c-keil/main.c
+++ c-keil/main.c
@@ -8,6 +8,7 @@
 * 
 * File: main.c
 * Contains high-level kobuki algorithm
+*
 */
 
 
@@ -36,146 +37,143 @@ int main() {
     US_Typedef object       = {};
 
     // For Storing States
-    int collected_rocks[2] = {0,0};
-    int DIR = CCW;
-		int prev_range=0;
+    int collected_rocks[2]  = {0,0};
+    int DIR                 = CCW;
+	int prev_range          = 0;
 
     // Boolean flag
-		int COLLECTING_FIRST_ROCK = collected_rocks[0] == FALSE;
-        int KEEP_SEARCHING = !(!COLLECTING_FIRST_ROCK || (COLLECTING_FIRST_ROCK && object.range<850)) ? FALSE : TRUE;
-		int LAYOUT_3 = FALSE;
-		int STOP_KOBUKI = FALSE;
-		int FOUND_NEW_ROCK = 0;
+    int collecting_first_rock   = collected_rocks[0] == FALSE;
+    int keep_searching          = (!collecting_first_rock || (collecting_first_rock && object.range<850)) ? TRUE : FALSE;
+    int LAYOUT_3                = FALSE;
+    int stop_kobuki             = FALSE;
+    int found_new_rock          = 0;
 		
     // Init
-    ADC_Init();
-    UART_Init();
-    Ultrasonic_Init();
+    ADCInit();
+    UARTInit();
+    UltrasonicInit();
 
 
     while(1) {
         
         // Update Sensor Values
-        Kobuki_Read(&kobuki);
-        Ultrasonic_Detect(&object);
+        KobukiRead(&kobuki);
+        UltrasonicDetect(&object);
         
         // Boolean Flags to Increase Readablility
-        STOP_KOBUKI = (
-                kobuki.button == BUTTON_1   
-                //kobuki.wheeldrop            
-            );
-        COLLECTING_FIRST_ROCK = collected_rocks[0] == FALSE;
-				FOUND_NEW_ROCK= !(object.range>=prev_range-200 && object.range<=prev_range+200);
+        stop_kobuki = (
+            kobuki.button == BUTTON_1   
+            kobuki.wheeldrop            
+        );
+        collecting_first_rock   = collected_rocks[0] == FALSE;
+		found_new_rock          = !(object.range>=prev_range-200 && object.range<=prev_range+200);
 					
-				
         switch (state)
         {
         case IDLE:
-            if (kobuki.button == BUTTON_0)
-            {
+            if (kobuki.button == BUTTON_0) {
                 state = SEARCH;
             }
             else {
-                Kobuki_Drive(STOP);
-								Kobuki_Rotate(STOP);
+                KobukiDrive(STOP);
+				KobukiRotate(STOP);
             }
             break;
-						
+    
         case SEARCH:
             if (kobuki.button == BUTTON_1) {
-                state=IDLE;
+                state = IDLE;
             }
-            else if (object.detected && FOUND_NEW_ROCK) {
-								prev_range=object.range;
-								Kobuki_Rotate(STOP);
+            else if (object.detected && found_new_rock) {
+				prev_range = object.range;
+				KobukiRotate(STOP);
                 state = FOUND;
             }
             else if (kobuki.bumper || kobuki.cliff) {
-								Kobuki_Rotate(STOP);
+				KobukiRotate(STOP);
                 state = OBSTACLE;
             }
-            else if (!object.detected || (object.detected && KEEP_SEARCHING)) {
-                Kobuki_Rotate(SEARCH_SPEED*DIR);
+            else if (!object.detected || (object.detected && keep_searching)) {
+                KobukiRotate(SEARCH_SPEED*DIR);
             }
             break;
 						
         case FOUND:
-            if (STOP_KOBUKI) {
+            if (stop_kobuki) {
                 state = IDLE;
             }
-						// Find closest rock
-            else if (!COLLECTING_FIRST_ROCK || (COLLECTING_FIRST_ROCK && object.range<850)) {
-                KEEP_SEARCHING=FALSE;
-								Kobuki_Rotate_Angle_To_Completion((int16_t)11*DIR, kobuki);
-								state = OBJECT;
+			// Find closest rock
+            else if (!collecting_first_rock || (collecting_first_rock && object.range<850)) {
+                keep_searching = FALSE;
+                KobukiRotateAngleToCompletion((int16_t)11*DIR, kobuki);
+                state = OBJECT;
             }
             else {
-                KEEP_SEARCHING=TRUE;
-                state=SEARCH;
+                keep_searching = TRUE;
+                state = SEARCH;
             }
             break;
 						
         case OBJECT:
-						if (STOP_KOBUKI) {
+			if (stop_kobuki) {
                 state = IDLE;
             }
 						
-						// Bumping 1st rock or stopping before 2nd rock
-						if (object.detected && !kobuki.bumper && !kobuki.cliff) {
-							// DRIVE TO OBJECT
-							if (kobuki.distance_complete == 0) {
-								Kobuki_Drive_Distance(kobuki.distance);
-							}
-							
-							
-							if (COLLECTING_FIRST_ROCK) {
-								Kobuki_Drive_Distance_Setpoint(kobuki.distance, object.range);
-							}
-							else {
-								Kobuki_Drive_Distance_Setpoint(kobuki.distance, object.range-60);
-								if (object.range <= 60) state=IDLE;
-								// state=IDLE;
-							}
-						}
-						if (kobuki.bumper) {
-							collected_rocks[0] = TRUE;
-							state=IDLE;
-						}
-						if (kobuki.cliff) {
-							if (!COLLECTING_FIRST_ROCK) {
-								LAYOUT_3=TRUE;
-							}
-							state=OBSTACLE;
-						}
-						if (!object.detected && !kobuki.bumper && !kobuki.cliff) {
-							state=SEARCH;
-						}
+            // Bumping 1st rock or stopping before 2nd rock
+            if (object.detected && !kobuki.bumper && !kobuki.cliff) {
+                // DRIVE TO OBJECT
+                if (kobuki.distance_complete == 0) {
+                    KobukiDriveDistance(kobuki.distance);
+                }
+                
+                
+                if (collecting_first_rock) {
+                    KobukiDriveDistanceSetpoint(kobuki.distance, object.range);
+                }
+                else {
+                    KobukiDriveDistanceSetpoint(kobuki.distance, object.range-60);
+                    if (object.range <= 60) state=IDLE;
+                }
+            }
+
+            if (kobuki.bumper) {
+                collected_rocks[0] = TRUE;
+                state = IDLE;
+            }
+            if (kobuki.cliff) {
+                if (!collecting_first_rock) {
+                    LAYOUT_3 = TRUE;
+                }
+                state = OBSTACLE;
+            }
+            if (!object.detected && !kobuki.bumper && !kobuki.cliff) {
+                state = SEARCH;
+            }
             break;
 						
         case OBSTACLE:
-					if (STOP_KOBUKI) {
+			if (stop_kobuki) {
                 state=IDLE;
             }
-					if (!kobuki.bumper && !kobuki.cliff) {
-						state=SEARCH;
-					}
-					else {
-						Kobuki_Drive_To_Completion((int16_t)-80, kobuki);
-						if (kobuki.cliff && !LAYOUT_3) {
-							Kobuki_Rotate_Angle_To_Completion((int16_t)60*CW, kobuki);
-							Kobuki_Drive_To_Completion((int16_t)250, kobuki);
-						}
-						else if (kobuki.cliff && LAYOUT_3) {
-							Kobuki_Rotate_Angle_To_Completion((int16_t)100*CW, kobuki);
-							Kobuki_Drive_To_Completion((int16_t)700, kobuki);
-							Kobuki_Rotate_Angle_To_Completion((int16_t)100*CW, kobuki);
-							Kobuki_Drive_To_Completion((int16_t)500, kobuki);
-						}
-						
-					}
-						
+            if (!kobuki.bumper && !kobuki.cliff) {
+                state=SEARCH;
+            }
+            else {
+                KobukiDriveToCompletion((int16_t)-80, kobuki);
+                if (kobuki.cliff && !LAYOUT_3) {
+                    KobukiRotateAngleToCompletion((int16_t)60*CW, kobuki);
+                    KobukiDriveToCompletion((int16_t)250, kobuki);
+                }
+                else if (kobuki.cliff && LAYOUT_3) {
+                    KobukiRotateAngleToCompletion((int16_t)100*CW, kobuki);
+                    KobukiDriveToCompletion((int16_t)700, kobuki);
+                    KobukiRotateAngleToCompletion((int16_t)100*CW, kobuki);
+                    KobukiDriveToCompletion((int16_t)500, kobuki);
+                }
+                
+            }		
             break;
         }
     }
-        
+
 }
