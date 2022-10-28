05a6bfa229194f5baf1ea4e2fc5b6eec307e6e26
one76 <michaeln2022@gmail.com>
2022-10-28 21:43:07 +1030

    C 3 - removed unnecessary files

--- c-keil/UART3.h
+++
@@ -1,8 +0,0 @@
-#ifndef UART3_H
-#define UART3_H
-
-data UART_Init();
-data UART_Rx(type);
-data UART_Tx();
-
-#endif
--- c-keil/delay.c
+++
@@ -1,6 +0,0 @@
-#include "delay.h"
-
-void DelayMs(uint32_t ms) {
-  ms *= (SystemCoreClock/10000);
-  while (ms--) { __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); }
-}
